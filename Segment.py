68fc8a332821382f1346219173b2ad9070b79301
mrgur <gurgenidze.margarita@gmail.com>
2018-04-30 21:22:08 +0300

    some fixes to process strings with digits correctly

--- Segment.py
+++ Segment.py
@@ -54,8 +54,7 @@ def first_word_hyp_existing (hashtag, my_dict):
 def Make_hyp (hashtag, my_dict):
 	word_hyp = first_word_hyp_existing(hashtag, my_dict)
 	unknown_finish = 0 #iterator of the end of substring with unknown word
-	alphabet =  ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
-
+	alphabet =  ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я','.',',','!','?']
 
 	if word_hyp == []:
 		if not (hashtag[0] in alphabet):
@@ -64,9 +63,9 @@ def Make_hyp (hashtag, my_dict):
 			while notinalph and (not (len(hashtag) == symb_seq)):
 				notinalph = (not (hashtag[symb_seq] in alphabet))
 				symb_seq+=1
-		
+			if (not (len(hashtag) == symb_seq)):
+				symb_seq-=1
 			word_hyp.append(hashtag[:symb_seq])
-	
 	
 	unknown_count = [0 for i in range (0, len(word_hyp))]
 	if word_hyp == []:
@@ -91,9 +90,7 @@ def Make_hyp (hashtag, my_dict):
 		flag = False 
 		it = 0
 		while it < len(word_hyp):
-			#print(word_hyp)
-		#for it in range (0,len(word_hyp)):
-			
+			#print(word_hyp)			
 			if (len(word_hyp[it])>6) or (unknown_count[it] > 3):
 				word_hyp.pop(it)
 				unknown_count.pop(it)
@@ -107,6 +104,13 @@ def Make_hyp (hashtag, my_dict):
 					
 				if ibegin < len(hashtag):
 					substring = hashtag[ibegin:]
+					if unknown_count[it] == 0:
+						boof = word_hyp[it][:]
+						boof.append(substring)
+						if not (boof in word_hyp):
+							word_hyp.append(boof)
+							unknown_count.append(0)
+							flag = True
 					#print(substring)
 					#print(boof)
 					unknown_finish = 0
@@ -117,19 +121,27 @@ def Make_hyp (hashtag, my_dict):
 							while notinalph  and (not (len(substring) == symb_seq)):
 								notinalph = not(substring[symb_seq] in alphabet)
 								symb_seq+=1
-		
+							if not (notinalph):
+								symb_seq-=1
 							word_hyp[it].append(substring[:symb_seq])
 							substring = substring[symb_seq:]
 							flag = True
-							#print(substring)
+
 							
 						else:
 							
 							first_variant = True
+							digit_started = False
 							if len(substring) >= 1:
 								for i in range (0, len(substring)):
 										subsubstring = substring[i:]
-										if not (first_word_hyp_existing(subsubstring, my_dict) == []):
+										if digit_started and subsubstring[0] in alphabet:
+											digit_started = False
+										
+										if not (first_word_hyp_existing(subsubstring, my_dict) == []) or (not(subsubstring[0] in alphabet) and not digit_started):
+											if not(subsubstring[0] in alphabet) and not digit_started:
+												digit_started = True
+
 											if (first_variant):
 												first_variant = False
 												mem = word_hyp[it][:]
@@ -138,13 +150,21 @@ def Make_hyp (hashtag, my_dict):
 											else:
 												boof2 = mem[:]
 												boof2.append(substring[:unknown_finish])
-												word_hyp.append(boof2)
-												unknown_count.append(unknown_count[it]+1)
+												if not(boof2 in word_hyp):
+													word_hyp.append(boof2)
+													unknown_count.append(unknown_count[it]+1)
 											flag = True
+
 										unknown_finish = i+1
-								if len(substring) == unknown_finish and first_variant:
+
+								boof = word_hyp[it][:]
+								boof.append(substring)
+								if len(substring) == unknown_finish and first_variant and not (boof in word_hyp):
 									word_hyp[it].append(substring)
 									unknown_count.append(unknown_count[it]+1)
+								elif (boof in word_hyp):
+									word_hyp.pop(it)
+									it-=1 
 									
 										
 							substring = substring[unknown_finish:]
@@ -162,6 +182,8 @@ def Make_hyp (hashtag, my_dict):
 								if not(boof2 in word_hyp):
 									word_hyp.append(boof2)
 									unknown_count.append(unknown_count[it])
+					
+
 			it+=1
 
 				#print(word_hyp)
@@ -179,19 +201,20 @@ def Make_hyp (hashtag, my_dict):
 	return segmentation
 
 
-#print(Make_hyp('1000000000извинений', Dict_creator.Dict_form('/home/corra/Documents/VKR/zaliznjak.txt')))
+# print(Make_hyp('22мая2017г', Dict_creator.Dict_form('/home/corra/Documents/VKR/zaliznjak.txt')))
 
 
 def segmentation_test(splitted, joined):
 
-	my_dict = Dict_creator.Dict_form('zaliznjak.txt')
+	my_dict = Dict_creator.Dict_form('/home/corra/Documents/VKR/zaliznjak.txt')
 	print('Dictionary was successfuly formed!')
 	
 	s = open(splitted)
 	j = open(joined)
+	b = open ('/home/corra/Documents/VKR/bad.txt', 'w')
 	total = 0
 	success = 0
-		
+	
 	for j_line in j:
 		s_line = s.readline()
 		total+=1
@@ -201,10 +224,12 @@ def segmentation_test(splitted, joined):
 
 		if s_line in Make_hyp(j_line, my_dict):
 			success+=1
-
+		else:
+			b.write(s_line + '\n')
+	b.close()
 	print("hashtags processed: ",total, " \n hashtags splitted correctly: ", success, "\n occurancy: ", (success/total)*100, "% \n")
 
 
-segmentation_test('splittedHashtagsTest.txt', 'joinedHashtagsTest.txt')
+segmentation_test('/home/corra/Documents/VKR/splittedHashtagsTest.txt', '/home/corra/Documents/VKR/joinedHashtagsTest.txt')
 
 	
