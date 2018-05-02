a42e34f4c15c73c63cb27b79b091600dbd7dd6a9
mrgur <gurgenidze.margarita@gmail.com>
2018-05-02 10:00:00 +0300

    Remove unused parameters

--- Methrics.py
+++ Methrics.py
@@ -5,22 +5,18 @@ def methric_count(segmentation_list):
 	stemmer = Mystem()
 	methric_list = []
 	functional_list = ['CONJ','COM','PR','PART','INTJ']
-	names_list = ['A','NUM','ANUM','APRO','S','SPRO']
 	methric = []
 	po = ParseOptions(verbosity=1)
 	po = ParseOptions(min_null_count=0, max_null_count=999)
 	for line in segmentation_list:
-		print(line)
 
 		unknown_words = 0
 		functional_words = 0
-		names = 0
-		#different_name_cases = []
 		number_of_words = 0
 		link_rate = 0
+		
 		number_of_words = len(line.split())
 
-		stemmer = Mystem()
 		grammar_information = stemmer.analyze(line)
 		grammar_information = grammar_information[:len(grammar_information)-1]
 		
@@ -32,7 +28,6 @@ def methric_count(segmentation_list):
 		while {'text' : ' '} in grammar_information:
 			grammar_information.pop(grammar_information.index({'text' : ' '}))
 		for info in grammar_information:
-			print(info)
 			if 'analysis' in info.keys():
 				if len( info['analysis'])==0:
 					unknown_words+=1
@@ -44,10 +39,10 @@ def methric_count(segmentation_list):
 					for f in functional_list:
 						if grammar.find(f+'=', 0,  5) > -1 or grammar.find(f+',', 0, 5) > -1:
 							functional_words+=1
-					for n in names_list:
-						if grammar.find(n+'=',  0, 5) > -1 or grammar.find(n+',',  0, 5) > -1:
-							names+=1
+							break
+							
 
-		methric_list.append([link_rate, unknown_words, functional_words, number_of_words])
+		methric_list.append([link_rate, unknown_words, (number_of_words - functional_words)/number_of_words])
 
 	return methric_list 
+
